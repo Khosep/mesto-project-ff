347fae16d2355965ee7d18093792de41bc7627b5
Khosep <khose007@yandex.ru>
2024-11-03 13:36:56 +0500

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // плагин для работы с html-файлами
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // плагин для удаления dist
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // плагин для объединения CSS файлов
+module.exports = {
+    entry: { main: './src/components/index.js' }, // точка входа
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+            publicPath: ''
+    },
+    mode: 'development', // добавили режим разработчика
+    devServer: {
+        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+        compress: true, // это ускорит загрузку в режиме разработки
+        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+        open: true // сайт будет открываться сам при запуске npm run dev
+    },
+    module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+        {
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+        },
+        {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        // переносит исходные файлы в конечную сборку в том же формате
+        type: 'asset/resource'
+        },
+        {
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader, css-loader и postcss-loader
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: { importLoaders: 1 }
+            },
+            'postcss-loader'
+            ]
+        }
+    ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({     //https://webpack.js.org/plugins/html-webpack-plugin/#basic-usage
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ]
+};
