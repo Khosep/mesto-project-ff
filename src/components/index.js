9b5a5e053a9a530135b8e3c0cb95551a06b0a6b7
Khosep <khose007@yandex.ru>
2024-11-19 21:33:06 +0500

    validate forms

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 import '../pages/index.css'; // для webpack (и убрать подключение css в index.html)
-
 import {createPartialCreateCard, deleteCard, handleLikeClick} from './card.js';
 import {openPopup, closePopup} from './modal.js';
 import {initialCards} from './cards.js';
+import {enableValidation, clearValidation} from './validation.js';
 
 // Задаем постоянные (неизменяемые) параметры для partial функции создания карточки
 const partialCreateCard = createPartialCreateCard(deleteCard, handleOpenImagePopup, handleLikeClick);
@@ -25,6 +25,14 @@ const openImagePopup = document.querySelector('.popup_type_image');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
+// Определяем настройки по умолчанию
+const validationSettingsObj = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+};
 
 // РАБОТА С ФОРМАМИ
 
@@ -41,6 +49,7 @@ const cardLinkField= cardForm.link;
 const fillProfileForm = () => {
     profileNameField.value = profileTitle.textContent;
     profileDescriptionField.value = profileDescription.textContent;
+    //enableValidation(defaultSettingsObj);
 }
 
 // Обрабатываем отправку формы редактирования профиля
@@ -75,11 +84,16 @@ function handleOpenImagePopup(card) {
 // Обработчик клика на кнопку открытия модального окна для редактирования профиля
 editProfileButton.addEventListener('click', () => {
     fillProfileForm();
+    clearValidation(editProfilePopup, validationSettingsObj);
     openPopup(editProfilePopup);
 });
 
+
 // Обработчик события клика на кнопку открытия модального окна для добавления карточки
-addCartButton.addEventListener('click', () => openPopup(addCardPopup));
+addCartButton.addEventListener('click', () => {
+    clearValidation(addCardPopup, validationSettingsObj);
+    openPopup(addCardPopup);
+});
 
 // Обработчик события submit при редактировании профиля
 profileForm.addEventListener('submit', handleProfileFormSubmit);
@@ -89,3 +103,6 @@ cardForm.addEventListener('submit', handleCardFormSubmit);
 
 // Обработчики событий на действия внутри карточек (лайк, удаление, открытие картинки)
 //  находятся внутри функции createCard (см. модуль card.js)
+
+// Запускаем валидацию форм
+enableValidation(validationSettingsObj);
