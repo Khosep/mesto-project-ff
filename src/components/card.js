c32069a6a91ef03ca7e942af60516b748de25b34
Khosep <khose007@yandex.ru>
2024-11-24 17:42:40 +0500

    format files

--- src/components/card.js
+++ src/components/card.js
@@ -1,65 +1,94 @@
-import {deleteCardAPI, addLikeAPI, deleteLikeAPI} from './api.js';
+import { deleteCardAPI, addLikeAPI, deleteLikeAPI } from './api.js';
 
 const cardTemplate = document.querySelector('#card-template').content;
 
 // Функция для создания и добавления карточки
 // card - объект со свойствами 'id_, 'name', 'link', likes, 'owner', 'createdAt'
-function createCard (card, profileID, deleteCardFunc, openImageFunc, clickLikeFunc) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+function createCard(
+  card,
+  profileID,
+  deleteCardFunc,
+  openImageFunc,
+  clickLikeFunc
+) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__title');
-    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-    const cardLikeButton = cardElement.querySelector('.card__like-button');
-    const cardLikeCounter = cardElement.querySelector('.card__like-counter');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardLikeButton = cardElement.querySelector('.card__like-button');
+  const cardLikeCounter = cardElement.querySelector('.card__like-counter');
 
-    cardImage.src = card.link;
-    cardImage.alt = card.name;
-    cardTitle.textContent = card.name;
-    cardLikeCounter.textContent = card.likes.length;
-    // Кнопка удаления и обработка этой кнопки - только на свои карточки
-    if (card.owner._id === profileID) {
-        cardDeleteButton.addEventListener('click', () => deleteCardFunc(cardElement, card._id, deleteCardAPI));
-    } else {
-        cardDeleteButton.remove();
-    }
-    // Помечаем свои лайки цветом
-    if (card.likes.some(obj => obj._id === profileID)) {
-        cardLikeButton.classList.add('card__like-button_is-active');
-    }
-    cardImage.addEventListener('click', () => openImageFunc(card));
-    cardLikeButton.addEventListener('click', () => clickLikeFunc(cardLikeButton, card._id, cardLikeCounter, addLikeAPI, deleteLikeAPI));
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+  cardLikeCounter.textContent = card.likes.length;
+  // Кнопка удаления и обработка этой кнопки - только на свои карточки
+  if (card.owner._id === profileID) {
+    cardDeleteButton.addEventListener('click', () =>
+      deleteCardFunc(cardElement, card._id, deleteCardAPI)
+    );
+  } else {
+    cardDeleteButton.remove();
+  }
+  // Помечаем свои лайки цветом
+  if (card.likes.some((obj) => obj._id === profileID)) {
+    cardLikeButton.classList.add('card__like-button_is-active');
+  }
+  cardImage.addEventListener('click', () => openImageFunc(card));
+  cardLikeButton.addEventListener('click', () =>
+    clickLikeFunc(
+      cardLikeButton,
+      card._id,
+      cardLikeCounter,
+      addLikeAPI,
+      deleteLikeAPI
+    )
+  );
 
-    return cardElement;
+  return cardElement;
 }
 
 // ФУНКЦИИ, СВЯЗАННЫЕ С КАРТОЧКОЙ (ПЕРЕДАЮТСЯ В createCard)
 
-// 1. Обрабатываем клик по delete-кнопке (передается в функцию createCard)
+// 1. Обрабатывает клик по delete-кнопке (передается в функцию createCard)
 const deleteCard = async (cardElement, cardID, deleteCardAPI) => {
-    if (await deleteCardAPI(cardID)) {
-        cardElement.remove();
-    }
+  if (await deleteCardAPI(cardID)) {
+    cardElement.remove();
+  }
 };
 
-
-// 2. Обрабатываем клик по like-кнопке (передается в функцию createCard)
-const handleLikeClick = async (likeButton, cardID, likeCounter, addLikeAPI, deleteLikeAPI) => {
-    const classes = likeButton.classList;
-    const activeClass = 'card__like-button_is-active';
-    classes.toggle(activeClass);
-    const card =  classes.contains(activeClass) ? (await addLikeAPI(cardID)) : (await deleteLikeAPI(cardID));
-    likeCounter.textContent = card.likes.length
-}
+// 2. Обрабатывает клик по like-кнопке (передается в функцию createCard)
+const handleLikeClick = async (
+  likeButton,
+  cardID,
+  likeCounter,
+  addLikeAPI,
+  deleteLikeAPI
+) => {
+  const classes = likeButton.classList;
+  const activeClass = 'card__like-button_is-active';
+  classes.toggle(activeClass);
+  const card = classes.contains(activeClass)
+    ? await addLikeAPI(cardID)
+    : await deleteLikeAPI(cardID);
+  likeCounter.textContent = card.likes.length;
+};
 
 // 3. Обрабатываем открытие попапа с картинкой (передается в функцию createCard)
 // СОГЛАСНО ЗАДАНИЮ, перенесена в index.js
 
-// Создаем partial функцию
+// Создает partial функцию
 function createPartialCreateCard(deleteCardFunc, openImageFunc, clickLikeFunc) {
-    return function(card, profileID) {
-        return createCard (card, profileID, deleteCardFunc, openImageFunc, clickLikeFunc)
-    };
+  return function (card, profileID) {
+    return createCard(
+      card,
+      profileID,
+      deleteCardFunc,
+      openImageFunc,
+      clickLikeFunc
+    );
+  };
 }
 
-export {createPartialCreateCard, deleteCard, handleLikeClick};
+export { createPartialCreateCard, deleteCard, handleLikeClick };
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,24 @@
 import '../pages/index.css'; // для webpack (и убрать подключение css в index.html)
-import {createPartialCreateCard, deleteCard, handleLikeClick} from './card.js';
-import {openPopup, closePopup} from './modal.js';
-import {enableValidation, clearValidation} from './validation.js';
-import {getAllDataAPI, editProfileDataAPI, createCardAPI, editAvatarAPI} from './api.js';
+import {
+  createPartialCreateCard,
+  deleteCard,
+  handleLikeClick,
+} from './card.js';
+import { openPopup, closePopup } from './modal.js';
+import { enableValidation, clearValidation } from './validation.js';
+import {
+  getAllDataAPI,
+  editProfileDataAPI,
+  createCardAPI,
+  editAvatarAPI,
+} from './api.js';
 
 // Задаем постоянные (неизменяемые) параметры для partial функции создания карточки
-const partialCreateCard = createPartialCreateCard(deleteCard, handleOpenImagePopup, handleLikeClick);
+const partialCreateCard = createPartialCreateCard(
+  deleteCard,
+  handleOpenImagePopup,
+  handleLikeClick
+);
 // Место, куда вставляем карточки
 const placeList = document.querySelector('.places__list');
 
@@ -30,15 +43,15 @@ let profileID;
 // Переменные форм
 // Переменные для формы редактирования профиля
 const profileForm = document.forms['edit-profile'];
-const profileNameField= profileForm.name;
-const profileDescriptionField= profileForm.description;
+const profileNameField = profileForm.name;
+const profileDescriptionField = profileForm.description;
 // Переменные для формы редактирования профиля
 const avatarForm = document.forms['edit-avatar'];
-const avatarLinkField= avatarForm['avatar-link'];
+const avatarLinkField = avatarForm['avatar-link'];
 // Переменные для формы создания карточки
 const cardForm = document.forms['new-place'];
-const cardNameField= cardForm['place-name'];
-const cardLinkField= cardForm.link;
+const cardNameField = cardForm['place-name'];
+const cardLinkField = cardForm.link;
 
 // Настройки валидации
 const validationSettingsObj = {
@@ -53,122 +66,129 @@ const validationSettingsObj = {
 
 // Заполняет форму редактирования профиля текущими значениями
 const fillProfileForm = () => {
-    profileNameField.value = profileTitle.textContent;
-    profileDescriptionField.value = profileDescription.textContent;
-}
+  profileNameField.value = profileTitle.textContent;
+  profileDescriptionField.value = profileDescription.textContent;
+};
 
 // Обрабатывает отправку формы редактирования профиля (name и description)
 const handleProfileFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-        setLoadingStatus(editProfilePopup);
-        const remoteProfileData = await editProfileDataAPI(
-            {'name': profileNameField.value, 'about': profileDescriptionField.value}
-        );
-        placeProfileData(remoteProfileData, ['name', 'about']);
-        closePopup(editProfilePopup);
-    } catch (error) {
-        console.error('Error in handleProfileFormSubmit', error);
-    } finally {
-        setLoadingStatus(editProfilePopup, false);
-    }
+  event.preventDefault();
+  try {
+    setLoadingStatus(editProfilePopup);
+    const remoteProfileData = await editProfileDataAPI({
+      name: profileNameField.value,
+      about: profileDescriptionField.value,
+    });
+    placeProfileData(remoteProfileData, ['name', 'about']);
+    closePopup(editProfilePopup);
+  } catch (error) {
+    console.error('Error in handleProfileFormSubmit', error);
+  } finally {
+    setLoadingStatus(editProfilePopup, false);
+  }
 };
 
 // Обрабатывает отправку формы редактирования аватара
 const handleAvatarFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-        setLoadingStatus(editAvatarPopup);
-        const remoteProfileData = await editAvatarAPI(
-            {'avatar': avatarLinkField.value}
-        );
-        placeProfileData(remoteProfileData, ['avatar']);
-        closePopup(editAvatarPopup);
-    } catch (error) {
-        console.error('Error in handleAvatarFormSubmit', error);
-    } finally {
-        setLoadingStatus(editAvatarPopup, false);
-    }
+  event.preventDefault();
+  try {
+    setLoadingStatus(editAvatarPopup);
+    const remoteProfileData = await editAvatarAPI({
+      avatar: avatarLinkField.value,
+    });
+    placeProfileData(remoteProfileData, ['avatar']);
+    closePopup(editAvatarPopup);
+  } catch (error) {
+    console.error('Error in handleAvatarFormSubmit', error);
+  } finally {
+    setLoadingStatus(editAvatarPopup, false);
+  }
 };
 
 // Обрабатывает отправку формы создания карточки
 const handleCardFormSubmit = async (event) => {
-    event.preventDefault();
-    const card = {name: cardNameField.value, link: cardLinkField.value};
-    try {
-        setLoadingStatus(addCardPopup);
-        const remoteNewCard = await createCardAPI(card);
-        placeList.prepend(partialCreateCard(remoteNewCard, profileID));
-        closePopup(document.querySelector('.popup_type_new-card'));
-        cardForm.reset() // очищаем форму
-    } catch (error) {
-        console.error('Error in handleCardFormSubmit', error);
-    } finally {
-        setLoadingStatus(addCardPopup, false);
-    }
+  event.preventDefault();
+  const card = { name: cardNameField.value, link: cardLinkField.value };
+  try {
+    setLoadingStatus(addCardPopup);
+    const remoteNewCard = await createCardAPI(card);
+    placeList.prepend(partialCreateCard(remoteNewCard, profileID));
+    closePopup(document.querySelector('.popup_type_new-card'));
+    cardForm.reset(); // очищаем форму
+  } catch (error) {
+    console.error('Error in handleCardFormSubmit', error);
+  } finally {
+    setLoadingStatus(addCardPopup, false);
+  }
 };
 
 // Обрабатывает открытие попапа с картинкой (передается в функцию createCard)
 function handleOpenImagePopup(card) {
-    const imagePopup = document.querySelector('.popup__image');
-    const titlePopup = document.querySelector('.popup__caption');
-    imagePopup.src = card.link;
-    imagePopup.alt = titlePopup.textContent = card.name;
-    openPopup(openImagePopup);
+  const imagePopup = document.querySelector('.popup__image');
+  const titlePopup = document.querySelector('.popup__caption');
+  imagePopup.src = card.link;
+  imagePopup.alt = titlePopup.textContent = card.name;
+  openPopup(openImagePopup);
 }
 
 // Загружает карточки на страницу
 const placeCards = (cardsDataArray) => {
-    cardsDataArray.forEach((card) => placeList.append(partialCreateCard(card, profileID)));
+  cardsDataArray.forEach((card) =>
+    placeList.append(partialCreateCard(card, profileID))
+  );
 };
 
 // Загружает профиль на страницу
-const placeProfileData = (ProfileDataObj, props = ['name', 'about', 'avatar']) => {
-    props.includes('name') && (profileTitle.textContent = ProfileDataObj.name);
-    props.includes('about') && (profileDescription.textContent = ProfileDataObj.about);
-    props.includes('avatar') && (profileAvatar.style.backgroundImage = `url('${ProfileDataObj.avatar}')`);
+const placeProfileData = (
+  ProfileDataObj,
+  props = ['name', 'about', 'avatar']
+) => {
+  props.includes('name') && (profileTitle.textContent = ProfileDataObj.name);
+  props.includes('about') &&
+    (profileDescription.textContent = ProfileDataObj.about);
+  props.includes('avatar') &&
+    (profileAvatar.style.backgroundImage = `url('${ProfileDataObj.avatar}')`);
 };
 
 // Обрабатывает полученные с сервера данные и вставляет на страницу
 const handleFetchedInintialData = async (promise) => {
-    try {
-        const [cardsData, ProfileObj] = await promise;
-        // Записываем в глобальную переменную id профиля
-        profileID = ProfileObj._id;
-        const userID = ProfileObj._id;
-        placeCards(cardsData);
-        placeProfileData(ProfileObj);
-    } catch (error) {
-        console.error('Error in handleFetchedInintialData', error);
-    }
+  try {
+    const [cardsData, ProfileObj] = await promise;
+    // Записываем в глобальную переменную id профиля
+    profileID = ProfileObj._id;
+    const userID = ProfileObj._id;
+    placeCards(cardsData);
+    placeProfileData(ProfileObj);
+  } catch (error) {
+    console.error('Error in handleFetchedInintialData', error);
+  }
 };
 
 // Устанавливает статус 'Сохранение...' на кнопке (по умолчанию), либо 'Сохранение' (если false)
-function setLoadingStatus (popup, flag = true) {
-    const popupButton = popup.querySelector('.popup__button');
-    popupButton.textContent = flag ? 'Сохранение...' : 'Сохранить';
+function setLoadingStatus(popup, flag = true) {
+  const popupButton = popup.querySelector('.popup__button');
+  popupButton.textContent = flag ? 'Сохранение...' : 'Сохранить';
 }
 
 // ОБРАБОТЧИКИ СОБЫТИЙ
 
 // Обработчик клика на кнопку открытия модального окна для редактирования профиля
 editProfileButton.addEventListener('click', () => {
-    fillProfileForm();
-    clearValidation(editProfilePopup, validationSettingsObj);
-    openPopup(editProfilePopup);
+  fillProfileForm();
+  clearValidation(editProfilePopup, validationSettingsObj);
+  openPopup(editProfilePopup);
 });
 
 // Обработчик клика на кнопку открытия модального окна для редактирования аватара
 editAvatarButton.addEventListener('click', () => {
-    clearValidation(editAvatarPopup, validationSettingsObj);
-    openPopup(editAvatarPopup);
+  clearValidation(editAvatarPopup, validationSettingsObj);
+  openPopup(editAvatarPopup);
 });
-
 
 // Обработчик события клика на кнопку открытия модального окна для добавления карточки
 addCartButton.addEventListener('click', () => {
-    clearValidation(addCardPopup, validationSettingsObj);
-    openPopup(addCardPopup);
+  clearValidation(addCardPopup, validationSettingsObj);
+  openPopup(addCardPopup);
 });
 
 // Обработчик события submit при редактировании профиля
@@ -182,7 +202,6 @@ cardForm.addEventListener('submit', handleCardFormSubmit);
 
 // Обработчики событий на действия внутри карточек (лайк, удаление, открытие картинки)
 //  находятся внутри функции createCard (см. модуль card.js)
-
 
 // ДЕЙСТВИЯ
 
